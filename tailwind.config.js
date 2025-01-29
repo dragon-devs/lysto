bd5031a56ca6ed00a77e4896dcd5ea7a4452832a
DRH <dragonfourtyseven@gmail.com>
2025-01-30 03:42:41 +0500

    chore: selected all the configs

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      fontFamily: {
+        rubik: ["Rubik-Regular", "sans-serif"],
+        "rubik-bold": ["Rubik-Bold", "sans-serif"],
+        "rubik-medium": ["Rubik-Medium", "sans-serif"],
+        "rubik-light": ["Rubik-Light", "sans-serif"],
+        "rubik-extrabold": ["Rubik-ExtraBold", "sans-serif"],
+        "rubik-semibold": ["Rubik-SemiBold", "sans-serif"],
+      },
+      colors: {
+        "primary": {
+            100: "#020c1f",
+            200: "#031948",
+            300: "#1e5ade",
+            400: "#498eff",
+        },
+        accent: {
+            100: "#f2f2f2",
+        },
+        black: {
+          DEFAULT: "#000000",
+          100: "#8C8E98",
+          200: "#5b5b5b",
+          300: "#051026",
+        },
+        danger: "#c53515"
+      }
+    },
+  },
+  plugins: [],
+}
