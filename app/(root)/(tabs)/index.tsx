9567868b2d68aaeeada486d6f2c66e764ffba4dd
DRH <dragonfourtyseven@gmail.com>
2025-02-01 23:48:04 +0500

    chore: adding home screen header

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,16 +1,24 @@
-import { Text, View } from "react-native";
-import {Link} from "expo-router";
+import { Text, View, Image } from "react-native";
+import {SafeAreaView} from "react-native-safe-area-context";
+import images from "@/constants/images";
+import icons from "@/constants/icons";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="text-3xl font-rubik text-blue-500">Welcome to Lysto</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <View className="px-5">
+        <View className="flex flex-row items-center justify-between mt-5">
+          <View className="flex flex-row items-center">
+            <Image source={images.avatar} className="size-12 rounded-full" />
+            <View className="flex flex-col items-start ml-2 justify-center">
+              <Text className="text-xs font-rubik text-black-100">Good Morning</Text>
+              <Text className="text-base font-rubik-medium text-black-300">John Doe</Text>
+            </View>
+          </View>
+          <Image className="size-6" source={icons.bell} />
+        </View>
+      </View>
+
+    </SafeAreaView>
   );
 }
